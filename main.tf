18f2a2928e6a23f58c8f0b8a2e6c2ce7a8b432b3
sakshiksh <167436320+sakshiksh@users.noreply.github.com>
2024-07-04 13:13:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,28 +3,27 @@ terraform {
   required_version = "~> 1.1"
   required_providers {
     aws = {
-      version = "~>3.1"
+      source  = "hashicorp/aws"
+      version = "~> 3.1"
     }
   }
 }
+
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
   region = var.region_name
+  // No need to specify access_key and secret_key when using environment variables
+  // access_key = var.access_key
+  // secret_key = var.secret_key
 }
-variable "access_key"{
-   type = string
+
+variable "region_name" {
+  type    = string
+  default = "us-east-1"
 }
-variable "secret_key"{
-   type = string
-}
-variable "region_name"{
-   type = string
-   default = "us-east-1"
-}
+
 resource "aws_instance" "myec2" {
-  ami                    = "ami-06c68f701d8090592"
-  instance_type          = "t2.micro"
+  ami           = "ami-06c68f701d8090592"
+  instance_type = "t2.micro"
   tags = {
     Name = "terraform1"
   }
